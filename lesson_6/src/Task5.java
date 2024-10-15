3328b7675a7569fd116f97aa6217d2f728f7a8f3
Gatsaev Bilal <gatsaev705@gmail.com>
2024-10-15 23:39:57 +0300

    Домашнее задание №6, исправления №2

--- lesson_6/src/Task5.java
+++ lesson_6/src/Task5.java
@@ -1,10 +1,14 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Task5 {
     public static void main(String[] args) {
         String[] strings = new String[6];
+        HashSet<String> uniqueStrings = new HashSet<>();
+
         Scanner scanner = new Scanner(System.in);
 
+        // Ввод строк
         for (int i = 0; i < 6; i++) {
             System.out.print("Введите строку: ");
             strings[i] = scanner.nextLine();
@@ -12,17 +16,24 @@ public class Task5 {
 
         // Удаляем все вхождения повторяющихся строк, заменяя их на null
         for (int i = 0; i < strings.length; i++) {
-            for (int j = i + 1; j < strings.length; j++) {
-                if (strings[i] != null && strings[i].equals(strings[j])) {
-                    strings[j] = null;
+            if (uniqueStrings.contains(strings[i])) {
+                strings[i] = null; // Если строка уже есть в HashSet, заменяем на null
+            } else {
+                uniqueStrings.add(strings[i]); // Добавляем первое вхождение в HashSet
+                for (int j = i + 1; j < strings.length; j++) {
+                    if (strings[i] != null && strings[i].equals(strings[j])) {
+                        strings[j] = null; // Удаляем последующие повторения
+                    }
                 }
             }
-            strings[i] = null; // Первое вхождение также заменяем на null
         }
 
+        // Вывод строк после удаления повторений
         System.out.println("Строки после удаления повторений:");
         for (String string : strings) {
-            System.out.println(string);
+            if (string != null) {
+                System.out.println(string);
+            }
         }
     }
 }
